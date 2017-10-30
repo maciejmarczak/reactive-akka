0181b82865da31391712a2d7f2b259d3e099f062
Maciej Marczak <maciejmarczak@hotmail.com>
2017-10-30 09:41:50 +0100

    add missing message handlers

--- src/main/scala/org/mmarczak/reactive/store/Customer.scala
+++ src/main/scala/org/mmarczak/reactive/store/Customer.scala
@@ -1,13 +1,14 @@
 package org.mmarczak.reactive.store
 
-import akka.actor.{Actor, ActorRef, Props}
-import org.mmarczak.reactive.store.CustomerProtocol.{CheckoutStarted, PaymentServiceStarted}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import org.mmarczak.reactive.store.CheckoutProtocol.CheckoutClosed
+import org.mmarczak.reactive.store.CustomerProtocol.{CartEmpty, CheckoutStarted, PaymentConfirmed, PaymentServiceStarted}
 
 object Customer {
   def props(): Props = Props[Customer]
 }
 
-class Customer extends Actor {
+class Customer extends Actor with ActorLogging {
 
   private val cart = context.actorOf(Cart.props(), "cart")
 
@@ -17,6 +18,9 @@ class Customer extends Actor {
   override def receive: Receive = {
     case CheckoutStarted(checkoutActor) => checkout = Some(checkoutActor)
     case PaymentServiceStarted(paymentActor) => payment = Some(paymentActor)
+    case PaymentConfirmed => log.info("Payment confirmed.")
+    case CartEmpty => log.info("Cart empty.")
+    case CheckoutClosed => log.info("Checkout closed.")
   }
 
 }
